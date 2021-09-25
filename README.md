5bdc41a014727b53eeef45780b9b12c56bc77f13
Daniel Españadero <d.espanadero@gmail.com>
2021-09-25 13:55:36 +0200

    primer commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# TODO App
+
+Este un proyecto de lista de tareas (TO DO) realiado con JavaScript.
+
+### Notas:
+Recuerden reconstruir los módulos de Node
+```
+npm install
+```
+
+Y para construir el build, recueren:
+```
+npm run build
+```
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebPackPlugin       = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin    = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { SourceMapDevToolPlugin } = require("webpack")
+
+module.exports = {
+    mode: 'development',
+    optimization: {
+        minimizer: [ new OptimizeCssAssetsPlugin() ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: 'pre',
+                use: ['source-map-loader'],
+              },
+            {
+                test: /\.css$/,
+                exclude: /styles\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: { minimize: false }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name: 'assets/[name].[ext]'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+            new SourceMapDevToolPlugin({
+              filename: "[file].map"
+            }),
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
+    ]
+
+}
+
